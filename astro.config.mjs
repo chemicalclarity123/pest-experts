be015a7747acac68ac1b7f5ac199654e76f276f6
Matthew Jackson <louderthan@gmail.com>
2026-02-20 10:32:51 +0200

    chore: explicitly set astro output to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pestexperts.co.za',
+  output: 'static',
   adapter: cloudflare(),
   integrations: [sitemap({
     filter: (page) => !page.includes('/test-sanity')
